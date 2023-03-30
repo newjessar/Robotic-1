6e6eb128757e2ef8997fc5ddc2bf102085f9e0ba
Jay <you@example.com>
2023-03-31 01:22:14 +0200

    lab4, v12 - sObject det. works. next back detection

--- bot_spawner/scripts/bot_spawner.py
+++ bot_spawner/scripts/bot_spawner.py
@@ -18,14 +18,11 @@ def run_launch_file(bot_name, position):
 
 ## Add bot to spawn here via run_launch_file (bot_name, position)
 run_launch_file("bot1", 0)
-run_launch_file("bot2", 5)
-run_launch_file("bot2", 3)
-run_launch_file("bot5", 5)
-run_launch_file("bot5", 8)
-run_launch_file("bot5", 9)
+run_launch_file("bot3", 3)
+run_launch_file("bot4", 7)
 run_launch_file("bot6", 10)
-run_launch_file("bot3", 12)
-run_launch_file("bot4", 13)
+
+
 
 
 
